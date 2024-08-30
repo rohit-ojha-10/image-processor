0774050ca3e2a05f50d871a319ce8f181b241e69
Rohit <yorohit.2002@gmail.com>
2024-08-30 23:21:25 +0530

    connected to global redis client;

--- src/clients/redisClient.ts
+++ src/clients/redisClient.ts
@@ -1,7 +1,14 @@
-import { createClient } from "redis";
-export  const redisClient = createClient({
-    url: 'redis://localhost:6379'
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+dotenv.config();
+export const redisClient = createClient({
+    password: process.env.REDIS_PW,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: 18362
+    }
 });
+
 redisClient.connect().catch(console.error);
 
 // Ensure to handle client disconnection properly
